8374b994b5f0227cf4bf66db5657e128cd9185cc
Akihiko Yamaguchi <info@akihikoy.net>
2021-03-26 03:14:00 +0900

    Added a new example of multiprocessing;

--- python/basic/multiproc/multiproc6.py
+++ python/basic/multiproc/multiproc6.py
@@ -9,16 +9,18 @@ import Queue
 import random, copy
 
 def SubFunc(pid, queue_cmd, queue_out, parameter):
-  print 'Started:',pid,parameter
-  y= 0
-  for x in xrange(int(parameter*1e6)):
-    y+= x
+  print 'Started:',pid,parameter,id(parameter)
+  Y= []
+  for x in xrange(int(parameter[0]*1e6)):
+    if x%100==0:  Y.append(x)
+    else:         Y[-1]+= x
     try:
       cmd= queue_cmd.get(block=False)
       if cmd=='stop':  break
     except Queue.Empty:
       pass
-  queue_out.put((pid, parameter, x, y))
+  queue_out.put((pid, parameter, x, Y[-1]))
+  return pid, parameter, x, Y[-1]
 
 def Main0(param_list):
   param_list= copy.deepcopy(param_list)
@@ -27,18 +29,20 @@ def Main0(param_list):
   results= []
   while len(param_list)>0:
     p= param_list.pop(0)
-    SubFunc(0,queue_cmd,queue_out,p)
-    pid_out,parameter,x,y= queue_out.get()
+    print 'SubFunc request with id(p)',id(p)
+    #SubFunc(0,queue_cmd,queue_out,p)
+    #pid_out,parameter,x,y= queue_out.get()
+    pid_out,parameter,x,y= SubFunc(0,queue_cmd,queue_out,p)
     results.append((pid_out,parameter,x,y))
     print 'Finished:',results[-1]
 
   print 'Results:'
   for res in results:
-    print res
+    print res, id(res[1])
 
 def Main1(param_list):
   param_list= copy.deepcopy(param_list)
-  max_proc= 16
+  max_proc= 10
 
   queue_cmd= mp.Queue()
   queue_out= mp.Queue()
@@ -48,6 +52,7 @@ def Main1(param_list):
   while len(param_list)>0 or len(processes)>0:
     while len(param_list)>0 and len(processes)<max_proc:
       p= param_list.pop(0)
+      print 'SubFunc request with id(p)',id(p)
       new_proc= mp.Process(target=SubFunc, args=(pid,queue_cmd,queue_out,p))
       processes[pid]= new_proc
       processes[pid].start()
@@ -60,15 +65,15 @@ def Main1(param_list):
 
   print 'Results:'
   for res in results:
-    print res
+    print res, id(res[1])
 
 
 if __name__=='__main__':
-  param_list= [random.random() for _ in range(10)]
+  param_list= [[random.random(),0] for _ in range(10)]
   import time
-  t_start= time.time()
-  Main0(param_list)
-  print 'Main0 time:',time.time()-t_start
+  #t_start= time.time()
+  #Main0(param_list)
+  #print 'Main0 time:',time.time()-t_start
   print '====================='
   t_start= time.time()
   Main1(param_list)
