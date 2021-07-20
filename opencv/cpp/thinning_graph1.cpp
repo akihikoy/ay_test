abfe2fd8afa5d23899f61baee574e3c49e20c416
Akihiko Yamaguchi <info@akihikoy.net>
2021-07-20 12:44:11 +0900

    Introduced the Floyd's all-pairs-shortest-path (APSP) algorithm into thinning_graph1 to extract spine features (longest paths per sub-graph);

--- opencv/cpp/thinning_graph1.cpp
+++ opencv/cpp/thinning_graph1.cpp
@@ -280,7 +280,10 @@ TThinningGraph DetectNodesFromThinningImg(const cv::Mat &thinning_img, cv::Mat &
 // Follow the path starting at nitr->P to neitr->DP.
 // point_types: Thinning image where each point is categorized.
 // return the node index at the reached point, DP from the point, and path length;
-void FollowPathOnImg(const cv::Point p, int dp, const cv::Mat &point_types, int &next_node, int &dp_from_next, int &path_len)
+// If a pointer path_points is given, points on the path are stored into it, except for the start/end points.
+void FollowPathOnImg(const cv::Point p, int dp, const cv::Mat &point_types,
+    int &next_node, int &dp_from_next, int &path_len,
+    std::vector<cv::Point> *path_points=NULL)
 {
   path_len= 0;
   int dp_prev(dp);
@@ -293,62 +296,74 @@ void FollowPathOnImg(const cv::Point p, int dp, const cv::Mat &point_types, int 
     {
       next_node= type_nodeidx/10;
       dp_from_next= OppositeDP(dp);
+      ++path_len;
       return;
     }
   }
 
+  int DP_ORDER[]= {0, 1,3,5,7, 2,4,6,8};  // + (plus) neighbors -> x (corner) neighbors order.
   while(true)
   {
 // std::cerr<<" "<<px<<" "<<ExtendedAt<int>(point_types,px)<<" "<<dp_prev<<" ("<<OppositeDP(dp_prev)<<")"<<std::endl;
     px+= DPToOffset(dp_prev);
     ++path_len;
+    if(path_points!=NULL)  path_points->push_back(px);
     dp_prev= OppositeDP(dp_prev);
     int dp, type_nodeidx;
     TPointType type;
-    for(dp=1; dp<9; ++dp)  // 1,3,5,7: dp at + (plus) neighbors.
+    // for(dp=1; dp<9; ++dp)  // 1,3,5,7: dp at + (plus) neighbors.
+    bool found(false);
+    for(int dpo=1; dpo<9; ++dpo)
     {
-      if(dp==dp_prev)  continue;
+      dp= DP_ORDER[dpo];
+      if(dp==dp_prev || dp==CircularDP(dp_prev+1) || dp==CircularDP(dp_prev-1))  continue;
       type_nodeidx= ExtendedAt<int>(point_types,px+DPToOffset(dp));
       type= TPointType(type_nodeidx%10);
       if(type==ptTerminal || type==ptIntersection)
       {
         next_node= type_nodeidx/10;
         dp_from_next= OppositeDP(dp);
+        ++path_len;
         return;
       }
+      if(type==ptMiddleLine)
+      {
+        found= true;
+        break;
+      }
     }
+// //     bool found(false);
+// //     for(dp=1; dp<9; dp+=2)  // 1,3,5,7: dp at + (plus) neighbors.
+// //     {
+// //       if(dp==dp_prev)  continue;
+// //       if(TPointType(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10)==ptMiddleLine)
+// //       {
+// //         found= true;
+// //         break;
+// //       }
+// //     }
+// //     if(!found)
+// //     {
+// //       for(dp=2; dp<9; dp+=2)  // 2,4,6,8: dp at x (corner) neighbors.
+// //       {
+// //         if(dp==dp_prev)  continue;
+// //         if(TPointType(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10)==ptMiddleLine)
+// //         {
+// //           found= true;
+// //           break;
+// //         }
+// //       }
+// //     }
 //     bool found(false);
-//     for(dp=1; dp<9; dp+=2)  // 1,3,5,7: dp at + (plus) neighbors.
+//     for(dp=1; dp<9; ++dp)
 //     {
-//       if(dp==dp_prev)  continue;
+//       if(dp==dp_prev || dp==CircularDP(dp_prev+1) || dp==CircularDP(dp_prev-1))  continue;
 //       if(TPointType(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10)==ptMiddleLine)
 //       {
 //         found= true;
 //         break;
 //       }
 //     }
-//     if(!found)
-//     {
-//       for(dp=2; dp<9; dp+=2)  // 2,4,6,8: dp at x (corner) neighbors.
-//       {
-//         if(dp==dp_prev)  continue;
-//         if(TPointType(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10)==ptMiddleLine)
-//         {
-//           found= true;
-//           break;
-//         }
-//       }
-//     }
-    bool found(false);
-    for(dp=1; dp<9; ++dp)
-    {
-      if(dp==dp_prev || dp==CircularDP(dp_prev+1) || dp==CircularDP(dp_prev-1))  continue;
-      if(TPointType(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10)==ptMiddleLine)
-      {
-        found= true;
-        break;
-      }
-    }
     if(!found)  throw;
 // if(dp==9)  {std::cerr<<" all_neighbors:";for(int dp(1); dp<9; ++dp)std::cerr<<" "<<(ExtendedAt<int>(point_types,px+DPToOffset(dp))%10);std::cerr<<std::endl;}
     dp_prev= dp;
@@ -374,18 +389,100 @@ void ConnectNodes(TThinningGraph &graph, const cv::Mat &point_types)
 // std::cerr<<"path followed: "<<nitr->P<<", "<<neitr->DP<<" --> "<<graph.Nodes[i_next].P<<", "<<dp_from_next<<" / "<<path_len<<std::endl;
       neitr->Next= i_next;
       neitr->PathLen= path_len;
+      bool opposite_assigned(false);
       for(std::vector<TNeighbor>::iterator ne2itr(graph.Nodes[i_next].Neighbors.begin()),ne2itr_end(graph.Nodes[i_next].Neighbors.end()); ne2itr!=ne2itr_end; ++ne2itr)
         if(ne2itr->DP==dp_from_next)
         {
           ne2itr->Next= i_node;
           ne2itr->PathLen= path_len;
+          opposite_assigned= true;
         }
+      if(!opposite_assigned) std::cerr<<"###Failed opposite assignment: "<<i_node<<"-"<<neitr->DP<<"-->"<<dp_from_next<<"-"<<i_next<<std::endl;
     }
   }
 }
 //-------------------------------------------------------------------------------------------
 
-void DrawThinningGraph(cv::Mat &img, const TThinningGraph &graph)
+// Analyze a thinning graph to get spine features (longest path per graph).
+void ThinningGraphToSpinePolys(const TThinningGraph &graph, const cv::Mat &point_types, std::vector<std::vector<cv::Point> > &spine_polys, int poly_step=10)
+{
+  typedef TThinningGraph::TNode TNode;
+  typedef TThinningGraph::TNeighbor TNeighbor;
+  int N= graph.Nodes.size();  // Number of nodes.
+  std::list<TFloydAPSPEdge>  edges;  // List of edge and cost values: (node1,node2,cost).
+  int n(0);
+  for(std::vector<TNode>::const_iterator nitr(graph.Nodes.begin()),nitr_end(graph.Nodes.end()); nitr!=nitr_end; ++nitr,++n)
+    for(std::vector<TNeighbor>::const_iterator neitr(nitr->Neighbors.begin()),neitr_end(nitr->Neighbors.end()); neitr!=neitr_end; ++neitr)
+      edges.push_back(TFloydAPSPEdge(n,neitr->Next,neitr->PathLen));
+
+  //D: matrix containing lowest cost.
+  //P: matrix containing a via point on the shortest path.
+  cv::Mat D,P;
+  FloydAPSP(N, edges, D, P);
+  // std::cerr<<"Lowest cost matrix:"<<std::endl<<D<<std::endl;
+  // std::cerr<<"Via points on the shortest path matrix:"<<std::endl<<P<<std::endl;
+  std::vector<std::vector<int> > iso_graphs= IsolatedGraphs(P);
+  std::cerr<<"Isolated graphs: ";
+  for(int i(0),i_end(iso_graphs.size());i<i_end;++i)
+  {
+    std::cerr<<"[";
+    for(int j(0),j_end(iso_graphs[i].size());j<j_end;++j)
+      std::cerr<<" "<<iso_graphs[i][j];
+    std::cerr<<"]";
+  }
+  std::cout<<std::endl;
+
+  spine_polys.clear();
+  for(int i(0),i_end(iso_graphs.size());i<i_end;++i)
+  {
+    int n1_max(-1),n2_max(-1);
+    double path_len_max(-1);
+    for(int n1i(0),n1i_end(iso_graphs[i].size());n1i<n1i_end;++n1i)
+      for(int n2i(0),n2i_end(iso_graphs[i].size());n2i<n2i_end;++n2i)
+      {
+        int n1(iso_graphs[i][n1i]), n2(iso_graphs[i][n2i]);
+        if(D.at<double>(n1,n2)>path_len_max)
+        {
+          n1_max= n1;
+          n2_max= n2;
+          path_len_max= D.at<double>(n1,n2);
+        }
+      }
+    std::list<int> path;
+    bool res= ShortestPath(n1_max, n2_max, P, path);
+    std::cerr<<"Longest path in graph "<<i<<": ";
+    for(std::list<int>::const_iterator pitr(path.begin()), pitr_end(path.end()); pitr!=pitr_end; ++pitr) std::cerr<<" "<<*pitr;std::cerr<<std::endl;
+    if(!res || path.size()<=1)  continue;
+    std::vector<cv::Point> path_points;
+    std::list<int>::const_iterator pitr(path.begin()), pitr_end(path.end());
+    int n_prev(*pitr);
+    for(++pitr; pitr!=pitr_end; ++pitr)
+    {
+// std::cerr<<"n_prev "<<n_prev<<" / *pitr "<<*pitr<<std::endl;
+      path_points.push_back(graph.Nodes[n_prev].P);
+      int nei(0), nei_end(graph.Nodes[n_prev].Neighbors.size());
+      for(nei=0; nei<nei_end; ++nei)
+      {
+// std::cerr<<"  n_prev/next: "<<graph.Nodes[n_prev].Neighbors[nei].Next<<"("<<nei<<"/"<<nei_end<<")"<<std::endl;
+        if(graph.Nodes[n_prev].Neighbors[nei].Next==*pitr)  break;
+      }
+      if(nei==nei_end)  throw;
+      int i_next(-1), dp_from_next(-1), path_len(-1);
+      FollowPathOnImg(graph.Nodes[n_prev].P, graph.Nodes[n_prev].Neighbors[nei].DP, point_types, i_next, dp_from_next, path_len, &path_points);
+      if(i_next!=*pitr)  throw;
+      n_prev= *pitr;
+    }
+    path_points.push_back(graph.Nodes[path.back()].P);
+    std::vector<cv::Point> spine_poly;
+    for(int p(0),p_end(path_points.size()); p<p_end; p+= poly_step)
+      spine_poly.push_back(path_points[p]);
+    if(path_points.size()%poly_step!=0)  spine_poly.push_back(path_points.back());
+    spine_polys.push_back(spine_poly);
+  }
+}
+//-------------------------------------------------------------------------------------------
+
+void DrawThinningGraph(cv::Mat &img, const TThinningGraph &graph, const std::vector<std::vector<cv::Point> > &spine_polys)
 {
   typedef TThinningGraph::TNode TNode;
   typedef TThinningGraph::TNeighbor TNeighbor;
@@ -400,6 +497,7 @@ void DrawThinningGraph(cv::Mat &img, const TThinningGraph &graph)
     else
       cv::circle(img, nitr->P, 3, cv::Scalar(255,0,0));
   }
+  cv::polylines(img, spine_polys, /*isClosed=*/false, cv::Scalar(0,255,255), 2);
 }
 //-------------------------------------------------------------------------------------------
 
@@ -439,11 +537,14 @@ int main(int argc, char **argv)
 
     // Apply the graph point detection and connection.
     cv::Mat point_types_ZS, point_types_GH;
+    std::vector<std::vector<cv::Point> > spine_polys_ZS, spine_polys_GH;
     TThinningGraph graph_ZS= DetectNodesFromThinningImg(img_thinning_ZS, point_types_ZS);
     ConnectNodes(graph_ZS, point_types_ZS);
+    ThinningGraphToSpinePolys(graph_ZS, point_types_ZS, spine_polys_ZS);
     double t3= GetCurrentTime();
     TThinningGraph graph_GH= DetectNodesFromThinningImg(img_thinning_GH, point_types_GH);
     ConnectNodes(graph_GH, point_types_GH);
+    ThinningGraphToSpinePolys(graph_GH, point_types_GH, spine_polys_GH);
     double t4= GetCurrentTime();
 
     std::cout<<"Computation times:"<<endl
@@ -459,8 +560,8 @@ int main(int argc, char **argv)
     cv::mixChannels(in2, 3, &result_GH, 1, from_to, 3);
     result_ZS= 0.5*img + result_ZS;
     result_GH= 0.5*img + result_GH;
-    DrawThinningGraph(result_ZS, graph_ZS);
-    DrawThinningGraph(result_GH, graph_GH);
+    DrawThinningGraph(result_ZS, graph_ZS, spine_polys_ZS);
+    DrawThinningGraph(result_GH, graph_GH, spine_polys_GH);
     cv::imshow("Input", img_in);
     cv::imshow("Thinning ZHANGSUEN", result_ZS);
     cv::imshow("Thinning GUOHALL", result_GH);
