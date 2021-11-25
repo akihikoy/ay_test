342601394e6e06ca9935c1e2788ce706eee0c19c
Akihiko Yamaguchi <info@akihikoy.net>
2021-11-25 22:03:18 +0900

    Initial implementation of PyTorch utilities inspired by fastai;

---
+++ python/torch/ay_torch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#\file    ay_torch.py
+#\brief   PyTorch utility.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Nov.24, 2021
+import numpy as np
+import torch
+import torchvision
+import time
+import copy
+import matplotlib.pyplot as plt
+from PIL import Image as PILImage
+
+if __name__=='__main__':
+  pass
