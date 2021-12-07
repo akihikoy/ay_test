bca252b63ac5afb938cf161af46c02e7b1b2fc75
Akihiko Yamaguchi <info@akihikoy.net>
2021-12-07 16:25:53 +0900

    Minor updates;

--- opencv/python/depth2normal.py
+++ opencv/python/depth2normal.py
@@ -15,13 +15,17 @@ import six.moves.cPickle as pickle
 import cv2
 import time
 
-def DepthToNormalImg(img_depth):
+def DepthToNormalImg(img_depth, with_amp=False, amp_beta=5.0):
   m= img_depth.astype('int16')
   nx= np.pad((m[:,2:]-m[:,:-2])/2.0, ((0,0),(1,1)), 'constant')
   ny= np.pad((m[2:,:]-m[:-2,:])/2.0, ((1,1),(0,0)), 'constant')
   nz= np.zeros_like(nx)
-  print np.min(nx),np.max(nx), np.min(ny),np.max(ny)
-  return np.stack((nx,ny,nz)).transpose((1,2,0))
+  img_norm= np.stack((nx,ny,nz)).transpose((1,2,0))
+  if with_amp:
+    img_amp= np.linalg.norm(img_norm,axis=2)
+    img_amp= (np.arctan(img_amp/amp_beta))*512./np.pi
+    return img_norm, img_amp
+  return img_norm
 
 
 if __name__=='__main__':
@@ -37,10 +41,15 @@ if __name__=='__main__':
   print img_depth.shape, img_depth.dtype, [np.min(img_depth), np.max(img_depth)]
 
   t_start= time.time()
-  img_norm= DepthToNormalImg(img_depth)
+  img_norm,img_amp= DepthToNormalImg(img_depth, with_amp=True)
   print 'Computation time:',time.time()-t_start
+
+  print np.min(img_norm.reshape(-1,3),axis=0),np.max(img_norm.reshape(-1,3),axis=0)
+  print np.min(img_amp),np.max(img_amp)
 
   cv2.imshow('depth',cv2.cvtColor(img_depth.astype('uint8'), cv2.COLOR_GRAY2BGR))
   cv2.imshow('normal(abs)',np.abs(img_norm))
-  cv2.imshow('normal(normal)',np.linalg.norm(img_norm,axis=2)*0.25)
+  cv2.imshow('normal(amp)',img_amp.astype('uint8'))
+  img_amp= cv2.GaussianBlur(img_amp,(5,5),0)
+  cv2.imshow('normal(amp-blur)',img_amp.astype('uint8'))
   while cv2.waitKey() not in map(ord,[' ','q']):  pass
