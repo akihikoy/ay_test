918ab4cbfefedaad22aa5421de4736a9df1b865d
Akihiko Yamaguchi <info@akihikoy.net>
2022-03-08 17:22:49 +0900

    Modified the manual calibration adjuster program;

--- opencv/cpp/calib_man.cpp
+++ opencv/cpp/calib_man.cpp
@@ -40,89 +40,71 @@ void TCameraRectifier::Rectify(cv::Mat &frame, const cv::Scalar& border)
 }
 //-------------------------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------------------------
 #ifndef LIBRARY
+#define LIBRARY
+#include "float_trackbar.cpp"
+//-------------------------------------------------------------------------------------------
+
 double Alpha;
 cv::Mat K, D;
+int height, width;
 
-int Alpha_100, K00, K02, K11, K12, D0_1000, D1_1000, D2_1000, D3_1000, D4_1000;
 bool ParamChanged(false);
 
-void Map()
+template<typename T>
+void OnTrack(const TExtendedTrackbarInfo<T> &info, void*)
 {
-  #define R100(x)  (x*100.0)
-  #define R1000(x)  ((10.0+x)*1000.0)
-  Alpha_100= R100(Alpha);
-  K00= K.at<double>(0,0);
-  K02= K.at<double>(0,2);
-  K11= K.at<double>(1,1);
-  K12= K.at<double>(1,2);
-  D0_1000= R1000(D.at<double>(0,0));
-  D1_1000= R1000(D.at<double>(0,1));
-  D2_1000= R1000(D.at<double>(0,2));
-  D3_1000= R1000(D.at<double>(0,3));
-  D4_1000= R1000(D.at<double>(0,4));
-  #undef R100
-  #undef R1000
-}
-void InvMap(int,void*)
-{
-  #define R100(x)  ((double)x*0.01)
-  #define R1000(x)  ((double)x*0.001-10.0)
-  Alpha= R100(Alpha_100);
-  K.at<double>(0,0)= K00;
-  K.at<double>(0,2)= K02;
-  K.at<double>(1,1)= K11;
-  K.at<double>(1,2)= K12;
-  D.at<double>(0,0)= R1000(D0_1000);
-  D.at<double>(0,1)= R1000(D1_1000);
-  D.at<double>(0,2)= R1000(D2_1000);
-  D.at<double>(0,3)= R1000(D3_1000);
-  D.at<double>(0,4)= R1000(D4_1000);
-  #undef R100
-  #undef R1000
+  std::cerr<<"Changed: "<<info.Name<<": "<<info.Value<<std::endl;
   ParamChanged= true;
 }
 
-int main(int argc, char**argv)
+bool LoadCalib(const std::string &calib_filename)
 {
-  cv::FileStorage calib((argc>1)?(argv[1]):"calib/camera.yaml", cv::FileStorage::READ);
-  Alpha= (argc>2)?atof(argv[2]):1.0;
+  cv::FileStorage calib(calib_filename, cv::FileStorage::READ);
   if(!calib.isOpened())
   {
-    std::cerr<<"Failed to open calibration file"<<std::endl;
-    return -1;
+    std::cerr<<"Failed to open calibration file: "<<calib_filename<<std::endl;
+    return false;
   }
-  TCapture cap;
-  if(!cap.Open(((argc>3)?(argv[3]):"0"), /*width=*/((argc>4)?atoi(argv[4]):0), /*height=*/((argc>5)?atoi(argv[5]):0)))  return -1;
-
-  int height, width;
-  cv::Mat R(cv::Mat::eye(3, 3, CV_64F));
   calib["camera_matrix"] >> K;
   calib["distortion_coefficients"] >> D;
   calib["image_width"] >> width;
   calib["image_height"] >> height;
+  return true;
+}
+
+int main(int argc, char**argv)
+{
+  std::string calib_filename((argc>1)?(argv[1]):"calib/camera.yaml");
+  Alpha= (argc>2)?atof(argv[2]):1.0;
+  TCapture cap;
+  if(!cap.Open(((argc>3)?(argv[3]):"0"), /*width=*/((argc>4)?atoi(argv[4]):0), /*height=*/((argc>5)?atoi(argv[5]):0)))  return -1;
+
+  if(!LoadCalib(calib_filename))  return -1;
+  cv::Mat R(cv::Mat::eye(3, 3, CV_64F));
   TCameraRectifier cam_rectifier;
   cv::Size size_in(width,height), size_out(width,height);
   cam_rectifier.Setup(K, D, R, size_in, Alpha, size_out);
 
-  Map();
-
   cv::namedWindow("camera",1);
 
-  std::string win("trackbar");
-  cv::namedWindow(win,1);
-  cv::createTrackbar("Alpha_100", win, &Alpha_100, 200, InvMap);
-  cv::createTrackbar("K00", win, &K00, 5000, InvMap);
-  cv::createTrackbar("K02", win, &K02, 5000, InvMap);
-  cv::createTrackbar("K11", win, &K11, 5000, InvMap);
-  cv::createTrackbar("K12", win, &K12, 5000, InvMap);
-  cv::createTrackbar("D0_1000", win, &D0_1000, 100000, InvMap);
-  cv::createTrackbar("D1_1000", win, &D1_1000, 100000, InvMap);
-  cv::createTrackbar("D2_1000", win, &D2_1000, 100000, InvMap);
-  cv::createTrackbar("D3_1000", win, &D3_1000, 100000, InvMap);
-  cv::createTrackbar("D4_1000", win, &D4_1000, 100000, InvMap);
+  std::string window("trackbar");
+  cv::namedWindow(window,1);
+
+  CreateTrackbar<double>("Alpha", window, &Alpha, 0.0, 2.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("K00", window, &K.at<double>(0,0), 0.0f, 5000.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("K02", window, &K.at<double>(0,2), 0.0f, 5000.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("K11", window, &K.at<double>(1,1), 0.0f, 5000.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("K12", window, &K.at<double>(1,2), 0.0f, 5000.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("D0", window, &D.at<double>(0,0), -10.0, 10.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("D1", window, &D.at<double>(0,1), -10.0, 10.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("D2", window, &D.at<double>(0,2), -10.0, 10.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("D3", window, &D.at<double>(0,3), -10.0, 10.0, 1e-5,  &OnTrack);
+  CreateTrackbar<double>("D4", window, &D.at<double>(0,4), -10.0, 10.0, 1e-5,  &OnTrack);
 
   cv::Mat frame;
+  bool with_rectify(true);
   for(int i(0),i_saved(0);;++i)
   {
     if(!cap.Read(frame))
@@ -131,12 +113,17 @@ int main(int argc, char**argv)
       else break;
     }
     if(ParamChanged)
+    {
       cam_rectifier.Setup(K, D, R, size_in, Alpha, size_out);
-    cam_rectifier.Rectify(frame, /*border=*/cv::Scalar(0,0,0));
+      ParamChanged= false;
+    }
+    if(with_rectify)  cam_rectifier.Rectify(frame, /*border=*/cv::Scalar(0,0,0));
     cv::imshow("camera", frame);
     cv::imshow("trackbar", cv::Mat(1,1000,CV_64F));
     char c(cv::waitKey(1));
     if(c=='\x1b'||c=='q') break;
+    else if(c==' ')
+      with_rectify= !with_rectify;
     else if(c=='p')
     {
       cv::FileStorage fs("/dev/stdout", cv::FileStorage::WRITE);
@@ -144,7 +131,12 @@ int main(int argc, char**argv)
       fs<<"distortion_coefficients"<<D;
       fs.release();
     }
-    else if(c==' ')
+    else if(c=='l')
+    {
+      if(!LoadCalib(calib_filename))  return -1;
+      cam_rectifier.Setup(K, D, R, size_in, Alpha, size_out);
+    }
+    else if(c=='s')
     {
       std::stringstream file_name;
       file_name<<"/tmp/view"<<std::setfill('0')<<std::setw(4)<<(i_saved++)<<".png";
