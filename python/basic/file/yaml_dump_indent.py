1646a727776e82a8df7af9d285d7cd4b2e1e8c6e
Akihiko Yamaguchi <info@akihikoy.net>
2023-03-08 17:49:28 +0900

    Added a test code to correct the list indentation issue of Python YAML dump;

---
+++ python/basic/file/yaml_dump_indent.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#\file    yaml_dump_indent.py
+#\brief   Fix the incorrect indentation of lists in YAML.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Mar.8, 2023
+from yaml import load as yamlload
+from yaml import dump as yamldump
+try:
+  from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+  from yaml import Loader, Dumper
+
+from yaml import Dumper as yaml_Dumper
+
+class Dumper_IndentPlus(yaml_Dumper):
+  def increase_indent(self, flow=False, *args, **kwargs):
+    return super(Dumper_IndentPlus,self).increase_indent(flow=flow, indentless=False)
+
+if __name__=='__main__':
+  data= {
+    'A':{
+      'B':[
+        {'a':10,'b':20,'c':30},
+        {'a':100,'b':200,'c':300},
+        {'a':1000,'b':2000,'c':3000},
+        ]
+      }
+    }
+  print 'Dump with Dumper:'
+  print yamldump(data, Dumper=Dumper)
+
+  print 'Dump with Dumper_IndentPlus:'
+  print yamldump(data, Dumper=Dumper_IndentPlus)
