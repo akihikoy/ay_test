a4bb79507460e3f058d03cd0a3934aef806c4f54
Akihiko Yamaguchi <info@akihikoy.net>
2023-07-30 23:02:28 +0900

    Implemented PolygonIsClockwise which solves a bug of previous version used in ClipPolygon;

---
+++ python/geometry/polygon/polygon_clip3.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+#\file    polygon_clip3.py
+#\brief   Refactored version of ClipPolygon: extracted some subroutines;
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Jul.30, 2023
+
+from polygon_is_clockwise2 import PolygonIsClockwise
+import math,random
+
+#Clip subject_polygon by clip_polygon with Sutherland-Hodgman-Algorithm.
+#subject_polygon is an arbitrary polygon, clip_polygon is a convex polygon.
+#Output polygon is counterclockwise.
+#Ref: https://rosettacode.org/wiki/Sutherland-Hodgman_polygon_clipping
+def ClipPolygon(subject_polygon, clip_polygon):
+  def inside(p):
+    return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
+
+  def compute_intersection():
+    dc = [ cp1[0] - cp2[0], cp1[1] - cp2[1] ]
+    dp = [ s[0] - e[0], s[1] - e[1] ]
+    n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
+    n2 = s[0] * e[1] - s[1] * e[0]
+    n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
+    return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
+
+  ic = PolygonIsClockwise(subject_polygon)
+  if ic is None:  return []
+  if ic:  subject_polygon= list(reversed(subject_polygon))
+  ic = PolygonIsClockwise(clip_polygon)
+  if ic is None:  return []
+  if ic:  clip_polygon= list(reversed(clip_polygon))
+
+  output_list = subject_polygon
+  cp1 = clip_polygon[-1]
+  #print 'clip_polygon=',clip_polygon
+
+  for clip_vertex in clip_polygon:
+    cp2 = clip_vertex
+    input_list = output_list
+    output_list = []
+    #print 'input_list =',input_list
+    if len(input_list)==0:
+      return []
+    s = input_list[-1]
+    #print '  s=',s
+    #print '  cp1=',cp1
+    #print '  cp2=',cp2
+
+    for subject_vertex in input_list:
+      e = subject_vertex
+      #print '  e=',e,inside(e),inside(s)
+      if inside(e):
+        if not inside(s):
+          output_list.append(compute_intersection())
+        output_list.append(e)
+      elif inside(s):
+        output_list.append(compute_intersection())
+      s = e
+    cp1 = cp2
+  return(output_list)
+
+#Float version of range
+def FRange1(x1,x2,num_div):
+  return [x1+(x2-x1)*x/float(num_div) for x in range(num_div+1)]
+
+#Generate a random number of uniform distribution of specified bound.
+def Rand(xmin=-0.5,xmax=0.5):
+  return random.random()*(xmax-xmin)+xmin
+
+def Main():
+  #Generate polygon:
+  #polygon= [[50, 150], [200, 50], [350, 150], [350, 300],
+            #[250, 300], [200, 250], [150, 350], [100, 250],
+            #[100, 200]]
+  polygon= []
+  N= 100
+  for i in xrange(N):
+    th= (float(i)+Rand(-0.4,0.4))*2.0*math.pi/float(N)
+    r= Rand(1.0, 100.0)
+    x= r*math.cos(th)
+    y= r*math.sin(th)
+    polygon.append([x,y])
+
+  #rect= [[30.0,30.0],[30.0,-30.0],[-30.0,-30.0],[-30.0,30.0]]
+  #rect= [[10*(p[0]+50),10*(p[1]+50)] for p in rect]
+  #rect= [[30.0,30.0],[-30.0,30.0],[-30.0,-30.0],[30.0,-30.0]]
+  #rect= [[100, 100], [300, 100], [300, 300], [100, 300]]
+  #p1x= Rand(1.0, 100.0); p1y= Rand(1.0, 100.0); p2x= Rand(1.0, 100.0); p2y= Rand(1.0, 100.0)
+  p1x= Rand(-100.0, 100.0); p1y= Rand(-100, 100.0); p2x= Rand(-100, 100.0); p2y= Rand(-100, 100.0)
+  rect= [[p1x,p1y],[p2x,p1y],[p2x,p2y],[p1x,p2y]]
+  #rect= [[p1x,p1y],[p2x,p2y],[p1x,p1y],[p2x,p2y]]
+
+  #polygon= [[-1.69021, -87.6474],[-40.5151, -2.28697],[-25.867, 44.6599],[29.6493, -3.34294]]
+  #rect= [[-88.5993, 13.5659],[57.7085, 13.5659],[57.7085, -55.5519],[-88.5993, -55.5519]]
+  polygon= [[45.672222, -2.9928033],[1.6948826, 8.3420715],[-77.632523, 33.676598],[-29.458261, -45.078251]]
+  rect= [[-61.265263, -16.213083],[-37.288738, -16.213083],[-37.288738, 72.973518],[-61.265263, 72.973518]]
+
+  polygon2= ClipPolygon(polygon, rect)
+  #try:
+    #polygon2= ClipPolygon(polygon, rect)
+  #except Exception as e:
+    #print 'Error'
+    #print '  e: ',e
+    #print '  type: ',type(e)
+    #print '  args: ',e.args
+    #print '  message: ',e.message
+    #polygon2= []
+
+  def save_poly(poly,name):
+    fp= open('/tmp/'+name,'w')
+    for p in poly:
+      fp.write('{x} {y}\n'.format(x=p[0],y=p[1]))
+    if len(poly)>0:
+      fp.write('{x} {y}\n'.format(x=poly[0][0],y=poly[0][1]))  #closing loop
+    fp.close()
+  save_poly(polygon,'polygon.dat')
+  save_poly(rect,'clip_rect.dat')
+  save_poly(polygon2,'polygon_clipped.dat')
+
+
+def PlotGraphs():
+  print 'Plotting graphs..'
+  import os
+  commands=[
+    '''qplot -x2 aaa
+      /tmp/polygon.dat w l lw 2 t '"Original"'
+      /tmp/clip_rect.dat w l lw 2 t '"Rect"'
+      /tmp/polygon_clipped.dat w l lw 3 t '"Clipped"'
+      &''',
+    '''''',
+    '''''',
+    ]
+  for cmd in commands:
+    if cmd!='':
+      cmd= ' '.join(cmd.splitlines())
+      print '###',cmd
+      os.system(cmd)
+
+  print '##########################'
+  print '###Press enter to close###'
+  print '##########################'
+  raw_input()
+  os.system('qplot -x2kill aaa')
+
+if __name__=='__main__':
+  import sys
+  if len(sys.argv)>1 and sys.argv[1] in ('p','plot','Plot','PLOT'):
+    PlotGraphs()
+    sys.exit(0)
+  Main()
+
+  PlotGraphs()
+  sys.exit(0)
---
+++ python/geometry/polygon/polygon_is_clockwise.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+#\file    polygon_is_clockwise.py
+#\brief   Check if a polygon is clockwise.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Jul.30, 2023
+
+#WARNING: It is found that this algorithm is not robust in some cases.
+#  Use the method in polygon_is_clockwise2.
+#Check if a polygon is clockwise.
+#Exported from polygon_clip.ClipPolygon.
+def PolygonIsClockwise(polygon):
+  def is_left_of(edge_1, edge_2, test):
+    tmp1 = [edge_2[0] - edge_1[0], edge_2[1] - edge_1[1]]
+    tmp2 = [test[0] - edge_2[0], test[1] - edge_2[1]]
+    x = (tmp1[0] * tmp2[1]) - (tmp1[1] * tmp2[0])
+    if x < 0:  return False
+    elif x > 0:  return True
+    else:  return None  # Colinear points;
+
+  for p in polygon:
+    is_left = is_left_of(polygon[0], polygon[1], p);
+    if is_left != None:  #some of the points may be colinear.  That's ok as long as the overall is a polygon
+      return not is_left
+  return None #All the points in the polygon are colinear
+
+
+def Main():
+  import numpy as np
+  polygons=[
+    [[0.729,0.049],[0.723,0.082],[0.702,0.125],[0.682,0.124],[0.654,0.106],[0.656,0.101],[0.647,0.081],[0.652,0.078],[0.651,0.071],[0.655,0.071],[0.673,0.031]],
+    [[0.722,0.219],[0.717,0.220],[0.712,0.229],[0.693,0.235],[0.681,0.227],[0.672,0.230],[0.649,0.211],[0.637,0.213],[0.629,0.208],[0.626,0.216],[0.620,0.202],[0.616,0.203],[0.617,0.207],[0.609,0.200],[0.603,0.201],[0.601,0.191],[0.587,0.181],[0.589,0.175],[0.580,0.166],[0.585,0.133],[0.593,0.121],[0.605,0.113],[0.626,0.113],[0.645,0.121],[0.644,0.127],[0.651,0.123],[0.661,0.135],[0.669,0.134],[0.675,0.140],[0.702,0.148],[0.715,0.159],[0.717,0.150],[0.720,0.149],[0.721,0.167],[0.727,0.167],[0.730,0.195],[0.724,0.204]],
+    [[0.820,0.156],[0.793,0.154],[0.812,0.154],[0.812,0.150],[0.803,0.149],[0.806,0.134],[0.802,0.139],[0.796,0.133],[0.786,0.140],[0.779,0.139],[0.772,0.131],[0.774,0.126],[0.782,0.127],[0.779,0.134],[0.789,0.130],[0.788,0.115],[0.794,0.109],[0.773,0.111],[0.769,0.124],[0.755,0.143],[0.749,0.144],[0.753,0.150],[0.750,0.153],[0.737,0.147],[0.731,0.149],[0.738,0.141],[0.722,0.144],[0.722,0.124],[0.726,0.126],[0.729,0.123],[0.725,0.118],[0.733,0.107],[0.733,0.090],[0.738,0.086],[0.738,0.077],[0.740,0.082],[0.744,0.080],[0.749,0.041],[0.757,0.039],[0.758,0.032],[0.763,0.034],[0.762,0.040],[0.769,0.037],[0.769,0.008],[0.781,0.024],[0.778,0.034],[0.788,0.043],[0.828,0.144],[0.819,0.150]],
+    #[[0.6,0.05],[0.65,0.05],[0.65,0.1],[0.6,0.1]],
+    #[[0.6,0.15],[0.6,0.2],[0.65,0.15],[0.65,0.2]],
+    #[[0.65,0.05],[0.7,0.05],[0.7,0.1]],
+    #[[0.6,0.05],[0.6,0.1],[0.65,0.05],[0.65,0.1]],
+    ]
+
+  #Modify polygons
+  for i in np.random.choice(range(len(polygons)), size=np.random.choice(list(range(len(polygons))))):
+    polygons[i].reverse()
+  for i in range(len(polygons)):
+    i_split= np.random.choice(len(polygons[i]))
+    polygons[i]= polygons[i][i_split:]+polygons[i][:i_split]
+
+  def write_polygon(fp,polygon):
+    if len(polygon)>0:
+      d_cross= np.max(np.max(polygon,axis=0)-np.min(polygon,axis=0))*0.02
+      p0= np.array(polygon[0])
+      poly_cross= [p0, p0+[d_cross,d_cross], p0-[d_cross,d_cross], p0, p0+[-d_cross,d_cross], p0-[-d_cross,d_cross], p0]
+      for pt in poly_cross+polygon:
+        fp.write('%s\n'%' '.join(map(str,pt)))
+    fp.write('\n')
+
+  with open('/tmp/polygons1.dat','w') as fp1, open('/tmp/polygons2.dat','w') as fp2:
+    for polygon in polygons:
+      if PolygonIsClockwise(polygon):
+        write_polygon(fp1,polygon)
+      else:
+        write_polygon(fp2,polygon)
+
+def PlotGraphs():
+  print 'Plotting graphs..'
+  import os
+  commands=[
+    '''qplot -x2 aaa
+        /tmp/polygons1.dat u 1:2 lc 1 w l
+        /tmp/polygons2.dat u 1:2 lc 2 w l
+        &''',
+        #/tmp/polygons.dat u 1:2:-1 lc var w l
+    '''''',
+    '''''',
+    ]
+  for cmd in commands:
+    if cmd!='':
+      cmd= ' '.join(cmd.splitlines())
+      print '###',cmd
+      os.system(cmd)
+
+  print '##########################'
+  print '###Press enter to close###'
+  print '##########################'
+  raw_input()
+  os.system('qplot -x2kill aaa')
+
+if __name__=='__main__':
+  import sys
+  if len(sys.argv)>1 and sys.argv[1] in ('p','plot','Plot','PLOT'):
+    PlotGraphs()
+    sys.exit(0)
+  Main()
+
+  PlotGraphs()
+  sys.exit(0)
