51dd493541a3e2c557cc93f4700e97c3b098333e
Akihiko Yamaguchi <info@akihikoy.net>
2023-08-02 12:57:03 +0900

    Updated the find_first.py sample;

--- python/basic/find_first.py
+++ python/basic/find_first.py
@@ -4,8 +4,13 @@
 #\author  Akihiko Yamaguchi, info@akihikoy.net
 #\version 0.1
 #\date    Aug.26, 2021
+import random
 
 if __name__=='__main__':
-  l= range(10)
-  print next(i for i in l if i>0 and i%3==0)
+  l= range(int(random.random()*10))
+  print 'l=',l
+  try:
+    print next(i for i in l if i>0 and i%3==0)
+  except StopIteration:
+    print 'Not found'
 
