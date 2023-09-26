9ed2c8d6c6a2f7a3d0fbc06a5ab44e99cbac7f7e
Akihiko Yamaguchi <info@akihikoy.net>
2023-09-27 00:45:31 +0900

    Added a test code to find video devices;

---
+++ opencv/python/find_device_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#\file    find_device_1.py
+#\brief   Test of finding a video device (simple; just search for /dev/videoX).
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Sep.26, 2023
+from __future__ import print_function
+import glob
+import cv2
+
+if __name__=='__main__':
+  video_devs= []
+  for v_dev in glob.glob('/dev/video*'):
+    cap= cv2.VideoCapture(v_dev)
+    if cap.isOpened():
+      video_devs.append(v_dev)
+    cap.release()
+  print('Video devices: {}'.format(video_devs))
