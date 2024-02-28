b5356cbb12a16c8c5ec1aa6bf65f8591daed1ffb
Akihiko Yamaguchi <info@akihikoy.net>
2024-02-28 20:45:43 +0900

    Added test codes of ArUCo marker creation and detection with OpenCV;

---
+++ opencv/python/aruco_create.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#\file    aruco_create.py
+#\brief   Creating an ArUco marker.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Feb.28, 2024
+import cv2
+import sys
+
+if __name__=='__main__':
+  ID= int(sys.argv[1]) if len(sys.argv)>1 else 1
+  size= 200
+  borderBits= 1
+
+  dictionary= cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+  img= cv2.aruco.drawMarker(dictionary, ID, size, borderBits=1)
+
+  print 'Created a marker {}.'.format(ID)
+  print 'Press q on the window to quit.'
+  while(True):
+    cv2.imshow('marker_{}'.format(ID),img)
+    if cv2.waitKey(1)&0xFF==ord('q'):  break
+  cv2.destroyAllWindows()
+
+  filename= '../cpp/sample/marker/marker_{}.png'.format(ID)
+  cv2.imwrite(filename, img)
+  print 'Saved to:',filename
+
---
+++ opencv/python/aruco_detect.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#\file    aruco_detect.py
+#\brief   ArUco marker detection test.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Feb.28, 2024
+import cv2
+
+cap= cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+if __name__=='__main__':
+  dictionary= cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+  parameters= cv2.aruco.DetectorParameters_create()
+
+  while(True):
+    ret,frame= cap.read()
+
+    corners, ids, rejectedImgPoints= cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
+    cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+    print 'corners:', corners
+
+    cv2.imshow('marker_detection',frame)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+      break
+
+  cap.release()
+  cv2.destroyAllWindows()
