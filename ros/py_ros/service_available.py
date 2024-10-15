adaa51da2a5c0c183b57ba23ff4255c5804d29d8
Akihiko Yamaguchi <info@akihikoy.net>
2024-10-15 17:04:30 +0900

    Added test codes to check if ROS topic and service are available;

---
+++ ros/py_ros/service_available.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#\file    service_available.py
+#\brief   Function to check if a service is available.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Oct.15, 2024
+import roslib
+import rospy
+roslib.load_manifest('sensor_msgs')
+import sensor_msgs.msg
+import rosgraph
+
+def IsServiceAvailable(service_name):
+  try:
+    master= rosgraph.Master(rospy.get_name())
+    service_uri= master.lookupService(service_name)
+    return True
+  except rosgraph.MasterError:
+    return False
+  except Exception as e:
+    raise Exception('Error checking service availability: {}'.format(str(e)))
+
+if __name__=='__main__':
+  rospy.init_node('service_available')
+
+  service_name= '/gripper_driver/move'
+
+  print 'Testing IsServiceAvailable({})...'.format(service_name)
+  print ' result:',IsServiceAvailable(service_name)
+
+  service_name= '/gripper_driver/jump'
+
+  print 'Testing IsServiceAvailable({})...'.format(service_name)
+  print ' result:',IsServiceAvailable(service_name)
+
---
+++ ros/py_ros/topic_available.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#\file    topic_available.py
+#\brief   Function to check if a topic is available.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Oct.15, 2024
+import roslib
+import rospy
+roslib.load_manifest('sensor_msgs')
+import sensor_msgs.msg
+
+def TopicArrives(port_name, port_type, time_out=5.0):
+  try:
+    res= rospy.wait_for_message(port_name, port_type, time_out)
+    return res is not None
+  except rospy.ROSException:
+    return False
+
+def IsTopicAvailable(port_name, port_type):
+  try:
+    topics= rospy.get_published_topics()
+    for topic, topic_type in topics:
+      if topic==port_name and topic_type==port_type._type:
+        return True
+    return False  # Topic not found or type mismatch
+  except rospy.ROSException as e:
+    #rospy.logerr("Failed to get published topics: %s", str(e))
+    return False
+
+if __name__=='__main__':
+  rospy.init_node('topic_available')
+
+  port_name= '/joint_states'
+  port_type= sensor_msgs.msg.JointState
+
+  print 'Testing TopicArrives({},{})...'.format(port_name, port_type)
+  print ' result:',TopicArrives(port_name, port_type)
+
+  print 'Testing IsTopicAvailable({},{})...'.format(port_name, port_type)
+  print ' result:',IsTopicAvailable(port_name, port_type)
+
+  port_name= '/joint_states2'
+
+  print 'Testing TopicArrives({},{})...'.format(port_name, port_type)
+  print ' result:',TopicArrives(port_name, port_type)
+
+  print 'Testing IsTopicAvailable({},{})...'.format(port_name, port_type)
+  print ' result:',IsTopicAvailable(port_name, port_type)
+
