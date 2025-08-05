ca827d3ed1138a6df69f4d73db7178f743db65a8
Akihiko Yamaguchi <info@akihikoy.net>
2025-08-06 03:19:18 +0900

    Updated ros_sub_img.py;

--- ros/py_ros/ros_sub_img.py
+++ ros/py_ros/ros_sub_img.py
@@ -16,13 +16,13 @@ import sensor_msgs.msg
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
+img= None
+
 def ImageCallback(msg):
+  global img
   img= CvBridge().imgmsg_to_cv2(msg, "bgr8")
   img= cv2.flip(img, 1)
-  cv2.imshow('image', img)
-  if cv2.waitKey(1)&0xFF==ord('q'):
-    rospy.signal_shutdown('quit.')
-    cv2.destroyAllWindows()
+  print(f'Delay at ImgCb: {(rospy.Time.now().to_sec()-msg.header.stamp.to_sec())*1000:.3f}ms')
 
 def OnMouse(event, x, y, flags, param):
   if event==cv2.EVENT_LBUTTONDOWN:
@@ -32,5 +32,15 @@ if __name__=='__main__':
   cv2.namedWindow('image')
   cv2.setMouseCallback('image', OnMouse)
   rospy.init_node('ros_sub_img')
-  image_sub= rospy.Subscriber("/camera/color/image_raw", sensor_msgs.msg.Image, ImageCallback)
-  rospy.spin()
+  image_sub= rospy.Subscriber("/camera/color/image_raw", sensor_msgs.msg.Image, ImageCallback, queue_size=1)
+  #rospy.spin()
+
+  while not rospy.is_shutdown():
+    if img is None:
+      print(f'Image is not obtained yet')
+      rospy.sleep(0.1)
+      continue
+    cv2.imshow('image', img)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+      rospy.signal_shutdown('quit.')
+      cv2.destroyAllWindows()
